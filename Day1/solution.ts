910a9309dc855f923292c7e037a429042e9c80de
endre.berki <gonosztaviszorny@gmail.com>
2020-12-01 23:11:54 +0100

    Day 1 part 2 solution

--- Day1/solution.ts
+++ Day1/solution.ts
@@ -22,3 +22,30 @@ numbers.forEach(number => {
 
     complementers.add(target - number);
 });
+
+// Part two - three sum
+// O(n^2), O(n)
+
+interface SumPair {
+    firstPart: number,
+    secondPart: number
+}
+
+const threeSumComplementers = new Map<number, SumPair>();
+
+for (let i = 0; i < numbers.length; i++) {
+    for (let j = i+1; j < numbers.length; j++) {
+        const twoSumPart : number = numbers[i] + numbers[j];
+        threeSumComplementers.set(target - twoSumPart, { firstPart: numbers[i], secondPart: numbers[j] });
+    }
+}
+
+numbers.forEach(number => {
+    if(threeSumComplementers.has(number)) {
+        const others : SumPair | undefined = threeSumComplementers.get(number);
+        if(others) {
+            console.log(`Part 2 solution: ${number * others.firstPart * others.secondPart}`);    
+            return;
+        }
+    }
+});
