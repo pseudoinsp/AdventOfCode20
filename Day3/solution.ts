60679ef8c98d3cb8adf290b21dba3b0452ea85f4
endre.berki <gonosztaviszorny@gmail.com>
2020-12-03 13:56:05 +0100

    Day 3 part 2 solution

--- Day3/solution.ts
+++ Day3/solution.ts
@@ -3,16 +3,32 @@ import { readLines } from '../utils/inputReader';
 const lines : string[] = readLines('Day3\\input.txt');
 const lineLength = lines[0].length;
 
-let currentPosition: number[] = [0, 0];
-let treesOnLanding = 0;
+const traverseSlope = (xMotion: number, yMotion: number): number => {
+    let currentPosition: number[] = [0, 0];
+    let treesOnLanding = 0;
 
-for (let i = 0; i < lines.length; i++) {
-    const yPos = currentPosition[1] % lineLength;
-    const stateOnCoordinate = lines[i][yPos];
-    
-    treesOnLanding = treesOnLanding + (stateOnCoordinate == "#" ? 1: 0);
+    while(currentPosition[0] < lines.length)
+    {
+        const yPos = currentPosition[1] % lineLength;
+        const stateOnCoordinate = lines[currentPosition[0]][yPos];
+        
+        treesOnLanding += stateOnCoordinate == "#" ? 1 : 0;
 
-    currentPosition = [currentPosition[0] + 1, currentPosition[1] + 3];
-}
+        currentPosition = [currentPosition[0] + xMotion, currentPosition[1] + yMotion];
+    }   
 
-console.log(`Part 1 - Trees on landing part: ${treesOnLanding}`);
+    return treesOnLanding;
+};
+
+console.log(`Part 1 - Trees on landing part: ${traverseSlope(1, 3)}`);
+
+// Part 2
+const slopeVariants = [[1, 1], [1, 3], [1, 5], [1, 7], [2, 1]];
+
+let multipliedTrees = 1;
+slopeVariants.forEach(slope => {
+    const treesMetInThisSlope = traverseSlope(slope[0], slope[1]);
+    multipliedTrees *= treesMetInThisSlope;
+});
+
+console.log(`Part 2 - Multiplied trees on landing part: ${multipliedTrees}`);
