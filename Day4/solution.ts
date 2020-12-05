f159ffae95c3bba9c5eba8e92df1da4241cc1c7b
endre.berki <gonosztaviszorny@gmail.com>
2020-12-05 10:50:13 +0100

    Day 4 part 2 solution

--- Day4/solution.ts
+++ Day4/solution.ts
@@ -24,42 +24,62 @@ const processInput = (lines: string[]): Array<string> => {
 
 const passports = processInput(lines);
 
+const rules = new Map<string, (passportPartValue: string)=> boolean>();
+rules.set("byr", pp => {
+        const value = parseInt(pp);
+        return value >= 1920 && value <= 2002;
+    });
+rules.set("iyr", pp => {
+    const value = parseInt(pp);
+    return value >= 2010 && value <= 2020;
+});
+rules.set("eyr", pp => {
+    const value = parseInt(pp);
+    return value >= 2020 && value <= 2030;
+});
+rules.set("hgt", pp => {
+    const scale = pp.substring(pp.length - 2, pp.length).toString();
+    const value = parseInt(pp.substring(0, pp.length - 2));
+    if(scale === "cm")
+        return value >= 150 && value <= 193;
+    else if (scale === "in")
+        return value >= 59 && value <= 76;
+    return false;
+});
+rules.set("hcl", pp => {
+    if(pp[0] !== "#") return false;
+    const value = pp.substring(1);
+    if(value.length !== 6) return false;
+    return [...value].filter(c => (c >= "a" && c <= "f") || c === "0" || parseInt(c)).length === 6;
+});
+rules.set("ecl", pp => {
+    // should use set<string>
+    return pp === "amb" || pp === "blu" ||pp === "brn" ||pp === "gry" ||pp === "grn" ||pp === "hzl" || pp === "oth";
+});
+rules.set("pid", pp => {
+    if (pp.length !== 9) return false;
+    return [...pp].filter(x => x === "0" || parseInt(x)).length === 9;
+});
+
 const evaluatePassport = (passport: string): boolean => {
-    const parts = passport.split(' ');
-    const categories: string[] = parts.map(x => x.split(':')[0]);
-    return categories.length === 8 || (categories.length === 7) && !categories.includes("cid"); 
+    const parts: string[] = passport.split(' ');
+
+    if(parts.length < 7) return false;
+    if(parts.length === 7 && parts.map(x => x.split(':')[0]).includes("cid")) return false;
+
+    for(const part of parts) {
+        const partComponents = part.split(':');
+        const field = partComponents[0];
+        const value = partComponents[1];
+
+        const ruleForField = rules.get(field);
+        if(ruleForField && !ruleForField(value)) 
+            return false;
+    }
+
+    return true; 
 };
 
 const validPassports = passports.filter(evaluatePassport);
 
-console.log(`Part 1 - Valid passports: ${validPassports.length}`);
-
-// const requiredFields = new Set<string>();
-// requiredFields.add("byr");
-// requiredFields.add("iyr");
-// requiredFields.add("eyr");
-// requiredFields.add("hgt");
-// requiredFields.add("hcl");
-// requiredFields.add("ecl");
-// requiredFields.add("pid");
-
-// interface Passport {
-//     ecl: string
-//     pid: number
-//     eyr: number
-//     hcl: string
-//     byr: number
-//     iyr: number
-//     cid: number
-//     hgt: string
-// }
-
-// const parsePassports(lines: string[]): Array<Passport> => {
-//     lines.map(line => {
-//         const parts = line.split(' ');
-//         parts.
-//         return {
-//             ecl
-//         }
-//     })
-// }
+console.log(`Part 2 - Valid passports: ${validPassports.length}`);
