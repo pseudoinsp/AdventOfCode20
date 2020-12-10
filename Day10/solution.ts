d1794584f6ee8d93438a54b9ede072ae422ff7e2
endre.berki <gonosztaviszorny@gmail.com>
2020-12-10 23:15:46 +0100

    Day 10 part 2 solution

--- Day10/solution.ts
+++ Day10/solution.ts
@@ -5,7 +5,6 @@ const numbers : number[] = readLines('Day10\\input.txt').map(x => parseInt(x));
 numbers.push(0);
 
 // part 1
-
 const orderedNumbers = numbers.sort((a, b) => a - b);
 orderedNumbers.push(orderedNumbers[orderedNumbers.length - 1] + 3);
 
@@ -22,3 +21,25 @@ for (let i = 0; i < orderedNumbers.length - 1; i++) {
 }
 
 console.log(`Part 1 - Multiplied 1 and 3 diffs among the adapters: ${diffOf1 * diffOf3}`);
+
+// part 2
+
+const variationsUntilNthAdapter = new Array<number>(numbers.length);
+variationsUntilNthAdapter[0] = 1;
+variationsUntilNthAdapter[1] = 1;
+// the second adapter can be reached only from the first adapter, or from the first and the built-in adapter
+variationsUntilNthAdapter[2] = numbers[2] - numbers[0] <= 3 ? 2 : 1;
+
+for (let i = 3; i < numbers.length; i++) {
+    // this adapter can be reached from all tree previous adapter
+    if(numbers[i] - 3 === numbers[i-3])
+        variationsUntilNthAdapter[i] = variationsUntilNthAdapter[i-3]+variationsUntilNthAdapter[i-2]+variationsUntilNthAdapter[i-1];
+    // can be reached from the two previous numbers adapters
+    else if (numbers[i] - 2 === numbers[i-2])
+        variationsUntilNthAdapter[i] = variationsUntilNthAdapter[i-2]+variationsUntilNthAdapter[i-1];
+    // or only from the previous adapter
+    else
+        variationsUntilNthAdapter[i] = variationsUntilNthAdapter[i-1];
+}
+
+console.log(`Part 2: Adapter variations: ${variationsUntilNthAdapter[variationsUntilNthAdapter.length-1]}`);
