ade4d2497ef784cda68ef026eef1268f8d26bdbe
endre.berki <gonosztaviszorny@gmail.com>
2020-12-22 18:32:39 +0100

    Day 22 part 2 solution

--- Day22/solution.ts
+++ Day22/solution.ts
@@ -7,13 +7,35 @@ const decks = data.split("\r\n\r\n");
 const player1Deck = decks[0].split("\n").filter((_, i) => i !== 0).map(x => parseInt(x));
 const player2Deck = decks[1].split("\n").filter((_, i) => i !== 0).map(x => parseInt(x));
 
-const simulateGame = (player1Deck: number[], player2Deck: number[]): [number, number[]] => {
+const simulateGame = (gameId: number, player1Deck: number[], player2Deck: number[]): [number, number[]] => {
+    // let round = 1;
+    const earlierPlayer1Hashes = new Set<string>();
+    const earlierPlayer2Hashes = new Set<string>();
+
+
     while(player1Deck.length > 0 && player2Deck.length > 0) {
+        if(earlierPlayer1Hashes.has(JSON.stringify(player1Deck)) || earlierPlayer2Hashes.has(JSON.stringify(player2Deck)))
+            return [1, player1Deck];
+
+        earlierPlayer1Hashes.add(JSON.stringify(player1Deck));
+        earlierPlayer2Hashes.add(JSON.stringify(player2Deck));
+
         const player1Card = player1Deck.shift() || -1;
         const player2Card = player2Deck.shift() || -1;
 
-        const player1WonRound = player1Card > player2Card;
+        let player1WonRound = false;
+        // check for recursive game
+        if(player1Deck.length >= player1Card && player2Deck.length >= player2Card) {
+            const player1RecursiveDeck = player1Deck.slice(0, player1Card);
+            const player2RecursiveDeck = player2Deck.slice(0, player2Card);
+            const [ winningPlayer ] = simulateGame(gameId + 1, player1RecursiveDeck, player2RecursiveDeck);
+            player1WonRound = winningPlayer === 1;
+        }
+        else {
+            player1WonRound = player1Card > player2Card;
+        }
 
+        // console.log(`Game: ${gameId} round ${round} winner: ${player1WonRound ? "P1" : "P2"}`);
         if(player1WonRound) {
             player1Deck.push(player1Card);
             player1Deck.push(player2Card);
@@ -22,6 +44,8 @@ const simulateGame = (player1Deck: number[], player2Deck: number[]): [number, nu
             player2Deck.push(player2Card);
             player2Deck.push(player1Card);
         }
+
+        // round++;
     }
 
     if(player1Deck.length > 0)
@@ -30,6 +54,7 @@ const simulateGame = (player1Deck: number[], player2Deck: number[]): [number, nu
         return [2, player2Deck];
 };
 
-const [winner, winningDeck] = simulateGame(player1Deck, player2Deck);
+const [winner, winningDeck] = simulateGame(1, player1Deck, player2Deck);
 const winningScore = winningDeck.reduce((a, e, i) => a += e * (winningDeck.length - i), 0);
-console.log(`Part 1 - Winner: player ${winner}, deck: ${winningDeck.toString()} score: ${winningScore}`);
+
+console.log(`Part 2 - Winner: player ${winner}, deck: ${winningDeck.toString()} score: ${winningScore}`);
